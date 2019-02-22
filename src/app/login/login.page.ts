49a3832f87b5154436682f8013f2fbf7b147bdc5
fitch <francois-garcia@live.fr>
2019-02-22 15:04:58 +0100

    feat(sim) : add sim cordova plugin to login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Sim } from '@ionic-native/sim/ngx';
 import { AuthenticationService } from '../shared/authentication.service';
 
 declare var cordova: any;
@@ -16,9 +17,13 @@ export class LoginPage implements OnInit {
   user = {};
   firstName: string;
 
-  constructor(private router: Router, private authSvc: AuthenticationService) { }
+  constructor(private router: Router, private authSvc: AuthenticationService, private sim: Sim) { }
 
   ngOnInit() {
+    this.sim.getSimInfo().then(
+      (info) => console.log('Sim info: ', info),
+      (err) => console.log('Unable to get sim info: ', err)
+    );
   }
 
   logUser() {
